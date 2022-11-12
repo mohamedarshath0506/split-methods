dc0a927f2806748206b07301b92b660abcae8910
mohamedarshath0506 <mdarsath620@gmail.com>
2022-11-12 19:01:21 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+var method = "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled";
+console.log(method .split());
+console.log(method .split(''));
+console.log(method .split(' '));
+console.log(method .split(' , '));
+console.log(method .split('s'));
+console.log(method .split('a'));
+console.log(method .split('the'));
+console.log(method .split('industry'));
+console.log(method .split('ipsum'));
+console.log(method .split('printing'));
+console.log(method .split('text'));
+console.log(method .split('unknown'));
+console.log(method .split('of'));
+console.log(method .split('is'));
+console.log(method .split('and'));
+
+
